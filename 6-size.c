c78e30474d61dd6b0cad2259d0d713985e8d5ea7
Kathleen McKiernan <851@holbertonschool.com>
2019-09-19 02:39:38 +0000

    add 6

---
+++ 6-size.c
@@ -0,0 +1,15 @@
+/**
+*main - print sizes with printf
+*
+*return: 0
+*/
+#include <stdio.h>
+int main(void)
+{
+printf("Size of a char: %i byte(s)\n"
+printf("Size of an int: %i byte(s)\n"
+printf("Size of a long int: %i byte(s)\n"
+printf("Size of a long long int: %i byte(s)\n"
+printf("Size of a float: %i byte(s)\n"
+return (0);
+}
