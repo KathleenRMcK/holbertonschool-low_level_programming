a4abd935576ff97d38154b35e9dffaa7f4a13cb2
Kathleen McKiernan <851@holbertonschool.com>
2019-09-19 02:57:14 +0000

    add 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+/**
+ *main - print string with write
+ *
+ *return: 1
+ */
+#include <stdio.h>
+int main(void)
+{
+  write("and that piece of art is useful\" - Dora Korpar, \ 2015-10-19\n",);
+  return (1);
+}
