c585a842dc634eafa197ed6190a5460041713289
Kathleen McKiernan <851@holbertonschool.com>
2019-09-23 21:12:50 +0000

    add 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+/**
+ *main - print a to z with new line
+ *
+ *return: 0
+ */
+#include "holberton.h"
+void print_alphabet(void);
+char l
+for(l = 'a'; l >= 'z'; l++)
+  {
+    _putchar(l);
+    _putchar('/n');
+    return(0);
+  }
