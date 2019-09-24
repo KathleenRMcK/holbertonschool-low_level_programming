f845b353a05407dfb74b437cb20ffc244be62c6e
Kathleen McKiernan <851@holbertonschool.com>
2019-09-24 00:58:09 +0000

    debug 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,17 @@
 /**
- *main - print a to z and A to Z
+ * main - print a to z and A to Z
  *
- *return (0)
+ * Return: 0
  */
 #include <stdio.h>
 int main(void)
 {
-  int i;
-  int j;
-  for(i = 'a'; i <= 'z'; i++)
-    putchar (i);
-  for(j = 'A'; j <= 'Z'; j++)
-    putchar (j);
-  putchar ('\n');
-  return (0);
+int i;
+int j;
+for (i = 'a'; i <= 'z'; i++)
+putchar (i);
+for (j = 'A'; j <= 'Z'; j++)
+putchar (j);
+putchar ('\n');
+return (0);
 }
