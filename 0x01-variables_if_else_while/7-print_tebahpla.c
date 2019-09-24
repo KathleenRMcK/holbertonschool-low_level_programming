1412d6cf75f4edb3ba58fd6153e6693257e4ff9b
Kathleen McKiernan <851@holbertonschool.com>
2019-09-24 00:35:38 +0000

    debug 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,11 +7,8 @@
 int main(void)
 {
   int i;
-  for(i = 'z'; i => 'a'; i--)
+  for(i = 'z'; i >= 'a'; i--)
     putchar(i);
-  {
-  if(i == 'a')
     putchar('\n');
-  }
-  return(0);
+    return(0);
 }
