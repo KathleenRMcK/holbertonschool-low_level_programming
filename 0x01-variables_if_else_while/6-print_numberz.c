af075e5c7722a923f9eee6ea6fbb1bfb56060dad
Kathleen McKiernan <851@holbertonschool.com>
2019-09-24 01:13:50 +0000

    debug 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,19 @@
 /**
- *main - print string of single digit base 10 numbers
+ * main - print string of single digit base 10 numbers
  *
- *return: 0
+ * Return: 0
  */
 #include <stdio.h>
 int main(void)
 {
-  int i;
-  for(i = 48; i <= 57; i++)
-    {
-      putchar(i);
-      {
-	if(i == 57)
-	  putchar('\n');
-      }
-    }
-  return(0);
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+{
+if (i == 57)
+putchar('\n');
+}
+}
+return (0);
 }
