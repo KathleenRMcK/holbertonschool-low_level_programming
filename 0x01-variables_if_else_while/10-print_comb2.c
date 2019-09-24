6fe03a101bb7832429e0e688662c9415b9b21e08
Kathleen McKiernan <851@holbertonschool.com>
2019-09-24 02:51:40 +0000

    debug 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,16 +1,25 @@
-/**                                                       *main - print string from 00 to 99 with comma and space  *                                                        *return: 0                                               */
+/**
+ * main - print string from 00
+ * to 99 w/ comma and space
+ * Return: 0
+ */
 #include <stdio.h>
 int main(void)
 {
-  int i;
-  for(i = 48; i <= 57; i++)
-    putchar(i);
-  putchar(i);
-  if(i != 57)
-    {
-      putchar(',');
-      putchar(' ');
-    }
-  putchar('\n');
-  return(0);
+int i, j;
+for (i = 48; i <= 57; i++)
+{
+for (j = 48; j <= 57; j++)
+{
+putchar(i);
+putchar(j);
+if (i != 57 && j != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+}
+return (0);
 }
