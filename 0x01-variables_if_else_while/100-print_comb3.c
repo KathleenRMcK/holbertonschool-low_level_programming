2a7a1bc3e4a2ea10f83755d0e6a403a67a2c9b8f
Kathleen McKiernan <851@holbertonschool.com>
2019-09-24 03:25:00 +0000

    add 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+/**
+ * main - print all possible combos
+ *
+ * Return: output
+ */
+#include <stdio.h>
+int main(void)
+{
+int i, j;
+for (i = '0'; i <= '9'; i++)
+{
+for (j = i + 1; j <= '9'; j++)
+{
+putchar(i);
+putchar(j);
+if (i < '8')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
