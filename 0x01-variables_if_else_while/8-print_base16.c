af78ef1332feb80bacc1596ec29c5f3a3b940774
Kathleen McKiernan <851@holbertonschool.com>
2019-09-24 02:19:19 +0000

    debug 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,14 @@ int main(void)
 {
 int i;
 int j;
-for (i = 0; i < 10; i++)
+for (i = '0'; i <= '9'; i++)
+{
 putchar(i);
-for (j = 'a'; j < 'g'; j++)
+}
+for (j = 'a'; j <= 'f'; j++)
+{
 putchar(j);
+}
 putchar('\n');
 return (0);
 }
