0f5c7a32f41ebc82d45595d27c8874263245f90e
Kathleen McKiernan <851@holbertonschool.com>
2019-09-25 01:51:22 +0000

    debug 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,6 +10,7 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
+
 /**
  * i = 0;
  *
@@ -18,6 +19,7 @@ printf("Infinite loop incoming :(\n");
  * putchar(i);
  * }
  */
+
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
