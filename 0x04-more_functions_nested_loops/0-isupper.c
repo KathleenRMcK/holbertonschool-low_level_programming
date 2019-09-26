bf8ff6cc0aa7833508b5f51ed4bf673b0e2649b9
Kathleen McKiernan <851@holbertonschool.com>
2019-09-26 13:57:00 +0000

    add 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+/**
+ * _isupper - check for uppercase character
+ *
+ * Return: 1 if upper
+ */
+#include "holberton.h"
+int _isupper(int c)
+{
+if (c = 65; c <= 90; c++)
+return (1);
+else
+return (0);
+}
