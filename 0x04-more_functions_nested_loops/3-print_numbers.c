f88f3b92ebd719055db8edd9d6308067e1fc5971
Kathleen McKiernan <851@holbertonschool.com>
2019-09-26 15:04:36 +0000

    add 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+/**
+ * print_numbers - print string from 0 to 9
+ *
+ * Return: Output
+ */
+#include "holberton.h"
+void print_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+_putchar(c);
+_putchar('\n');
+}
