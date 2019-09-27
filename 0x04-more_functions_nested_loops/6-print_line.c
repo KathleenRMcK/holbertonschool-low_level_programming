37cb588474b925e9ccf15eec8b0d2230c09bc0cc
Kathleen McKiernan <851@holbertonschool.com>
2019-09-27 01:51:01 +0000

    add 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+/**
+ * print_line - print _ n times
+ * @n: length of line
+ * Return: Output
+ */
+#include "holberton.h"
+void print_line(int n)
+{
+int i;
+n = 95;
+i = 1;
+while (i <= n)
+i++;
+_putchar(n);
+if (n <= 0)
+{
+_putchar('\n');
+}
+_putchar('\n');
+}
