9df70b6994bc0921904b4a26b57b59082a34ce2d
Kathleen McKiernan <851@holbertonschool.com>
2019-09-27 01:55:00 +0000

    debug 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,7 @@
 void print_line(int n)
 {
 int i;
-n = 95;
-i = 1;
-while (i <= n)
-i++;
-_putchar(n);
-if (n <= 0)
-{
-_putchar('\n');
-}
+for (i = 1; i <= n; i++)
+_putchar(95);
 _putchar('\n');
 }
