a549ba7425d665a2016c17f084ecc2c68a5191f4
Kathleen McKiernan <851@holbertonschool.com>
2019-09-27 01:43:28 +0000

    debug 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,11 @@ int j;
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 14; j++)
-if (j >= 10)
 {
-_putchar(j / 10);
+if (j > 9)
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
 }
-_putchar(j % 10);
 _putchar('\n');
 }
 }
