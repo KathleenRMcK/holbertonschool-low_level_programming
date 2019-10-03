75666dafd64e07dcdb6934556d2662ceb87a3327
Kathleen McKiernan <851@holbertonschool.com>
2019-10-03 18:36:57 +0000

    add 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,3 +6,17 @@
  */
 #include "holberton.h"
 void reverse_array(int *a, int n)
+{
+	int l, m, temp;
+
+	l = 0;
+	m = n / 2;
+	n = n - 1;
+	while (l < m)
+	{
+		temp = a[l];
+		a[l] = a[n - l];
+		a[n - l] = temp;
+		l += 1;
+	}
+}
