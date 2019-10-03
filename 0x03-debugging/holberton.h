f26b741b98150cf9b22c34829e034220cd68ca69
Kathleen McKiernan <851@holbertonschool.com>
2019-10-03 13:42:47 +0000

    debug header

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -7,5 +7,5 @@ int convert_day(int month, int day);
 int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
 void print_remaining_days(int month, int day, int year);
-#endif /* HOLBERTON_H */
 
+#endif
