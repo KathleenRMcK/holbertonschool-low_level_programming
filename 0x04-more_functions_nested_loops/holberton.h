643847eb28ce8e12099fcb6ac17e669f9fe3ef29
Kathleen McKiernan <851@holbertonschool.com>
2019-10-03 13:46:27 +0000

    debug header

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 #include <unistd.h>
 
 int _putchar(char c);
@@ -11,3 +14,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+
+#endif
