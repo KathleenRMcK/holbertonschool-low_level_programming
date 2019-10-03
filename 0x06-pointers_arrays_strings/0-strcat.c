d58f35523d2ad7eb7252e1982d53d37daf9edd7f
Kathleen McKiernan <851@holbertonschool.com>
2019-10-03 15:11:22 +0000

    add 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,3 +6,19 @@
  */
 #include "holberton.h"
 char *_strcat(char *dest, char *src)
+{
+	int l = 0;
+	int m = 0;
+
+	while (dest[l] != '\0')
+	{
+		l += 1;
+	}
+	while (src[m] != '\0')
+	{
+		dest[l] = src[m];
+		l += 1;
+		m += 1;
+	}
+	return (dest);
+}
