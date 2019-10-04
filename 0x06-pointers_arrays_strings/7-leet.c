fa9fd2b3b51defead0c258b10373dec6e4f5e372
Kathleen McKiernan <851@holbertonschool.com>
2019-10-04 01:59:10 +0000

    add 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,33 @@
 /**
  * leet - encodes string into 1337
- *
+ * @str: string being printed
  * Return: Output
  */
 #include "holberton.h"
-char *leet(char *)
+char *leet(char *str)
+{
+	int l;
+
+	for (l = 0; str[l] != '\0'; l++)
+	if (str[l] == 'a' || str[l] == 'A')
+	{
+		str[l] = '4';
+	}
+	if (l == 'e' || l == 'E')
+	{
+		str[l] = '3';
+	}
+	if (l == 'o' || l == 'O')
+	{
+		str[l] = '0';
+	}
+	if (l == 't' || l == 'T')
+	{
+		str[l] = '7';
+	}
+	if (l == 'l' || l == 'L')
+	{
+		str[l] = '1';
+	}
+	return (str);
+}
