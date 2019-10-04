23fc819e41a8693fff4f56a670e5adc37effd19a
Kathleen McKiernan <851@holbertonschool.com>
2019-10-04 00:57:23 +0000

    debug 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,3 +7,20 @@
  */
 #include "holberton.h"
 char *_strncat(char *dest, char *src, int n)
+{
+	int l, m;
+
+	l = 0;
+	m = 0;
+	while (dest[l] != '\0')
+	{
+		l += 1;
+	}
+	while (src[m] != '\0' && m < n)
+	{
+		dest[l] = src[m];
+		l += 1;
+		m += 1;
+	}
+	return (dest);
+}
