8b05abfc1a3f6b07f95139876ce3afda7a472ea3
Kathleen McKiernan <851@holbertonschool.com>
2019-10-07 23:55:50 +0000

    add 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,3 +6,18 @@
  */
 #include "holberton.h"
 unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int m;
+	unsigned int n;
+	unsigned int o;
+
+	for (m = 0; s[m] > ' '; m++)
+	{
+		for (n = 0; accept[n] != '\0'; n++)
+		if (accept[n] == s[m])
+		{
+			o++;
+		}
+	}
+		return (o);
+}
