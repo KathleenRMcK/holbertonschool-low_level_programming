7efc5653a0a56c490e5cdbb30982429ebaf43217
Kathleen McKiernan <851@holbertonschool.com>
2019-10-08 00:05:01 +0000

    add 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,3 +6,16 @@
  */
 #include "holberton.h"
 char *_strpbrk(char *s, char *accept)
+{
+	int l, m;
+
+	for (l = 0; s[l] != '\0'; l++)
+	{
+		for (m = 0; accept[m] != '\0'; m++)
+			if (accept[m] == s[l])
+			{
+				return (&s[l]);
+			}
+	}
+	return (0);
+}
