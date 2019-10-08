51c09846e7c28819272196ba767b18e13abb88d6
Kathleen McKiernan <851@holbertonschool.com>
2019-10-08 00:38:34 +0000

    add 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,3 +6,17 @@
  */
 #include "holberton.h"
 char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+	while (*haystack != '\0' && *needle != '\0' && *haystack == *needle)
+	{
+		needle++;
+		haystack++;
+	}
+	if (*needle == '\0')
+		haystack += 1;
+	return (haystack);
+	}
+	return (NULL);
+}
