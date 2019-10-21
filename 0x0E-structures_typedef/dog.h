21ab9430950a2741c5f040b288bc0e9344b38914
Kathleen McKiernan <851@holbertonschool.com>
2019-10-21 21:51:18 +0000

    update header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-int _putchar(char c)
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+/**
+ * struct dog - struct about dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
