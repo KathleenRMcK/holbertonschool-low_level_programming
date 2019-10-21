0e6d34a39bb982f4d857b2c8ae428ab5c66f4650
Kathleen McKiernan <851@holbertonschool.com>
2019-10-21 23:32:19 +0000

    add 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -5,4 +5,13 @@
  * Return: 0
  */
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
