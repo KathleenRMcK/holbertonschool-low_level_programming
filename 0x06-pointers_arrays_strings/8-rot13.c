c2ad486c56bb95f05ee4923e77eca3d194fda2eb
Kathleen McKiernan <851@holbertonschool.com>
2019-10-22 04:04:02 +0000

    add 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,7 +1,22 @@
 /**
  * rot13 - encodes string using rot13
- *
+ * @s: string being printed
  * Return: Output
  */
 #include "holberton.h"
-char *rot13(char *)
+char *rot13(char *s)
+{
+	int l;
+
+	for (l = 0; s[l] != '\0'; l++)
+	if ((s[l] >= 'n' && s[l] <= 'z') || (s[l] >= 'N' && s[l] <= 'Z'))
+	{
+		s[l] = (s[l] + 13);
+	}
+	else
+	while ((s[l] >= 'n' && s[l] <= 'z') || (s[l] >= 'N' && s[l] <= 'Z'))
+	{
+		s[l] = (s[l] - 13);
+	}
+	return (s);
+}
