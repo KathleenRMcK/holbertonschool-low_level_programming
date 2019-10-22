0997c3188625b7a3271a5ec214aff8546566ebed
Kathleen McKiernan <851@holbertonschool.com>
2019-10-22 04:07:59 +0000

    debug 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -9,11 +9,10 @@ char *rot13(char *s)
 	int l;
 
 	for (l = 0; s[l] != '\0'; l++)
-	if ((s[l] >= 'n' && s[l] <= 'z') || (s[l] >= 'N' && s[l] <= 'Z'))
+	if ((s[l] >= 'a' && s[l] <= 'm') || (s[l] >= 'A' && s[l] <= 'M'))
 	{
 		s[l] = (s[l] + 13);
 	}
-	else
 	while ((s[l] >= 'n' && s[l] <= 'z') || (s[l] >= 'N' && s[l] <= 'Z'))
 	{
 		s[l] = (s[l] - 13);
