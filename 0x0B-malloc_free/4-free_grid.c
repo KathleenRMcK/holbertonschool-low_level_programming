f777773a4d1083e79a402f786c5f4121ae493c02
Kathleen McKiernan <851@holbertonschool.com>
2019-10-22 01:43:45 +0000

    add 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,14 @@
 /**
- *
- *
- *
+ * free_grid - free allocated 2D array
+ * @grid: grid being freed
+ * @height: height of array
  */
 #include "holberton.h"
 void free_grid(int **grid, int height)
+{
+	int l;
+
+	for (l = 0; l < height; l++)
+		free(grid[l]);
+	free(grid);
+}
