b8052d10092b9f98562d3c2288c4179ee5d2f8ff
Kathleen McKiernan <851@holbertonschool.com>
2019-10-22 03:08:58 +0000

    add 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -6,3 +6,15 @@
  */
 #include "holberton.h"
 int *array_range(int min, int max)
+{
+	int l, m, *twod;
+
+	if (min > max)
+		return (NULL);
+	twod = malloc(sizeof(int) * (max - min + 1));
+	if (twod == NULL)
+		return (NULL);
+	for (l = 0, m = min; m <= max; l++, m++)
+		twod[l] = m;
+	return (twod);
+}
