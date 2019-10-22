a5f47065cd2aac2803278c2f329ea0badfb19dc9
Kathleen McKiernan <851@holbertonschool.com>
2019-10-22 04:15:55 +0000

    add 100

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -5,3 +5,17 @@
  */
 #include "holberton.h"
 void print_number(int n)
+{
+	unsigned int prints;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		prints = -n;
+	}
+	else
+		prints = n;
+	if (prints / 10)
+		print_number(prints / 10);
+	_putchar(prints % 10 + '0');
+}
