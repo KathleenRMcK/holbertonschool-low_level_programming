a9407fe3ce61adb00362d3e055d1366914086f4b
Kathleen McKiernan <851@holbertonschool.com>
2019-10-22 01:09:19 +0000

    add 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,45 @@
 /**
- *
- *
- *
+ * _strlen - return length of string
+ * @s: string being measured
+ * Return: length of string
+ */
+#include "holberton.h"
+int _strlen(char *s)
+{
+	int t = 0;
+
+	while (s[t] != '\0')
+	{
+		t += 1;
+	}
+	return (t);
+}
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: 0
  */
 #include "holberton.h"
 char *str_concat(char *s1, char *s2)
+{
+	int l, sone, stwo;
+	char *catstr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	sone = _strlen(s1);
+	stwo = _strlen(s2);
+	catstr = malloc(sizeof(char) * (sone + stwo + 1));
+	if (catstr == NULL)
+		return (NULL);
+	for (l = 0; l < sone; l++)
+		catstr[l] = s1[l];
+	for (; l < sone + stwo; l++)
+		catstr[l] = s2[l - sone];
+	catstr[sone + stwo] = '\0';
+	return (catstr);
+}
