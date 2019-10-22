7b03766949e6225f5f42859b7c2cc7c1d43f256b
Kathleen McKiernan <851@holbertonschool.com>
2019-10-22 02:04:00 +0000

    add 5

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,7 +1,45 @@
 /**
- *
- *
- *
+ * _strlen - return length of string
+ * @s: string being measured
+ * Return: length of string
  */
 #include "holberton.h"
-void free_grid(int **grid, int height)
+int _strlen(char *s)
+{
+	int t = 0;
+	while (s[t] != '\0')
+	{
+		t += 1;
+	}
+	return (t);
+}
+
+/**
+ * argstostr - concatenates all arguments
+ * @ac: argument counter
+ * @av: argument vector
+ * Return: 0
+ */
+#include "holberton.h"
+char *argstostr(int ac, char **av)
+{
+	int l, m;
+	char *catstr;
+	int start = 0, length = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (l = 0; l < ac; l++)
+		length += _strlen(av[l]);
+	catstr = malloc(sizeof(char) * (length + ac + 1));
+	if (catstr == NULL)
+		return (NULL);
+	for (l = 0; l < ac; l++)
+	{
+		for (m = 0; av[l][m] != '\0'; m++)
+			catstr[start++] = av [l][m];
+		catstr[start++] = '\n';
+	}
+	catstr[start] = '\0';
+	return (catstr);
+}
