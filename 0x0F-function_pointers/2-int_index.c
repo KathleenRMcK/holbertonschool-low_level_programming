6ec046bcc0289dd9a71a1dda5c8baa259bc9f521
Kathleen McKiernan <851@holbertonschool.com>
2019-10-23 17:27:17 +0000

    add 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,3 +7,15 @@
  */
 #include "function_pointers.h"
 int int_index(int *array, int size, int (*cmp)(int))
+{
+	int l;
+
+	if (array == NULL || (*cmp) == NULL || size <= 0)
+		return (-1);
+	for (l = 0; l < size; l++)
+		if ((*cmp)(array[l]))
+		{
+			return (l);
+		}
+	return (-1);
+}
