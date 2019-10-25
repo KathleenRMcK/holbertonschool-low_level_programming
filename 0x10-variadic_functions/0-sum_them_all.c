dc4a133310a9e50d5a91c5b7bffe34e591bc2410
Kathleen McKiernan <851@holbertonschool.com>
2019-10-25 13:54:40 +0000

    add 0x10 file structure, header, main

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,10 @@
+/**
+ *
+ *
+ *
+ */
+#include "variadic_functions.h"
+int sum_them_all(const unsigned int n, ...);
+{
+	if (n == 0)
+		return (0);
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,10 @@
+/**
+ *
+ *
+ *
+ */
+#include "variadic_functions.h"
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	if (seperator == NULL)
+		return;
