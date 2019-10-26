f7fab1928a2693226bd057efe72d84ec8202e9a9
Kathleen McKiernan <851@holbertonschool.com>
2019-10-26 20:22:32 +0000

    sync with remote

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,10 +1,24 @@
 /**
- *
- *
- *
+ * sum_them_all - sum of all parameters
+ * @n: numbers
+ * Return: Output
  */
 #include "variadic_functions.h"
-int sum_them_all(const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...)
 {
+	va_list list;
+	unsigned int l;
+	int sum;
+
 	if (n == 0)
 		return (0);
+	sum = 0;
+
+	va_start(list, n);
+	for (l = 0; l < n; l++)
+
+	sum += va_arg(list, int);
+
+	va_end(list);
+	return (sum);
+}
