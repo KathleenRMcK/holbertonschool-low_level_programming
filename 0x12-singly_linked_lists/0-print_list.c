71d639136171ab206d7af2517963450195038897
Kathleen McKiernan <851@holbertonschool.com>
2019-10-31 19:14:07 +0000

    add 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,19 +1,20 @@
 /**
- * 
- *
- *
+ * print_list - print elements of list
+ * @h: list being printed
+ * Return: Output
  */
 #include "lists.h"
 size_t print_list(const list_t *h)
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+	int l;
 
-	while (h != NULL)
+	for (l = 0; h != NULL; l++)
 	{
-		printf("%d -> ", h -> str);
-		       h = h -> next;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
 	}
-	printf("[0] (nil)");
-} list_t;
+	return (l);
+}
