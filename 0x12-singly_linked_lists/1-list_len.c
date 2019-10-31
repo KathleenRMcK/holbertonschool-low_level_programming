2e05a60f7ee5f0aa08da9ee9bdececb2448be35f
Kathleen McKiernan <851@holbertonschool.com>
2019-10-31 19:14:22 +0000

    add 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,19 @@
-returns number of nodes
+/**
+ * list_len - return number of nodes
+ * @h: list being checked
+ * Return: Output
+ */
+#include "lists.h"
+size_t list_len(const list_t *h)
+{
+	int l;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		h = h->next;
+		l++;
+	}
+	return (l);
+}
