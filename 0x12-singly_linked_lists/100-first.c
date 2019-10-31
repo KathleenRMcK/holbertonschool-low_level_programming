0479a86e9d41ff01e07838a3e6d87a130198c041
Kathleen McKiernan <851@holbertonschool.com>
2019-10-31 21:26:14 +0000

    add 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+/**
+ * first - message from rabbit and the hare
+ * Description: Print message before main executes
+ *
+ */
+#include "lists.h"
+void first(void) __attribute__ ((constructor));
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
