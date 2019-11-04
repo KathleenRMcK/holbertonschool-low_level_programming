ce0b7b6c211a5b3ea5b362434f1dbe07d9f26148
Kathleen McKiernan <851@holbertonschool.com>
2019-11-04 19:45:21 +0000

    add atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,30 @@
-/**
- * _atoi - convert string to integer
- * @s: string being converted
- * Return: Output
- */
 #include "holberton.h"
+/**
+ * _atoi - convert string to int
+ * @s: string to convert
+ * Return: Outcome
+ */
 int _atoi(char *s)
+{
+	int n;
+	int pos;
+	int neg;
+
+	n = 0;
+	pos = 0;
+	neg = 1;
+	if (*s == '\0')
+		return (0);
+	else if (s[0] == '-')
+	{
+		neg = -1;
+		n++;
+	}
+	for (; s[n] != '\0'; n++)
+	{
+		if (s[n] <= '0' || s[n] >= '9')
+			return (0);
+		pos = pos * 10 + s[n] - '0';
+	}
+	return (neg * pos);
+}
