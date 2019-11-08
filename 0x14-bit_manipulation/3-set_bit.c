968b78cf8576a1bfac3f2958013bad680ec79234
Kathleen McKiernan <851@holbertonschool.com>
2019-11-08 03:40:49 +0000

    fix for betty

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,7 +1,8 @@
 /**
- *
- *
- *
+ * set_bit - sets bit at given index to 1
+ * @n: number being changed
+ * @index: index being swapped
+ * Return: Output
  */
 #include "holberton.h"
 int set_bit(unsigned long int *n, unsigned int index)
