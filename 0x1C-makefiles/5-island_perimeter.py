e224faecaf0bd2c1bc471334441b898129046b69
Kathleen McKiernan <kathleenmckiernan@UofNs-iMac-3.local>
2020-07-07 17:56:09 -0400

    initial sync from remote for checker

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Island Perimeter
+"""
+
+
+def island_perimeter(grid):
+    """ Function to find length of perimeter of island """
+    perimeter = 0
+    for lon in range(len(grid)):
+        for lat in range(len(grid[lon])):
+            if grid[lon][lat] == 1:
+                if lon == 0 or lat == 0:
+                    perimeter = perimeter + 1
+                if grid[lon - 1][lat] == 0:
+                    perimeter = perimeter + 1
+                if grid[lon][lat - 1] == 0:
+                    perimeter = perimeter + 1
+                if grid[lon + 1][lat] == 0:
+                    perimeter = perimeter + 1
+                if grid[lon][lat + 1] == 0:
+                    perimeter = perimeter + 1
+                if lon == len(grid) - 1:
+                    perimeter = perimeter + 1
+                if lat == len(grid[lon]) - 1:
+                    perimeter = perimeter + 1
+    return (perimeter)
---
+++ 0x1C-makefiles/README.md
@@ -0,0 +1,10 @@
+# 0x1C - Makefiles
+
+### Project Files
+0-Makefile |
+1-Makefile |
+2-Makefile |
+3-Makefile |
+4-Makefile |
+5-Makefile |
+100-Makefile |
