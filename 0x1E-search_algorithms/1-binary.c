7c5764bc5c0cfbeccade14390a6bf35848929753
Kathleen McKiernan <kathleenmckiernan@UofNs-iMac-3.local>
2020-11-09 14:13:42 -0500

    sync for checker

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,28 +10,28 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-        int parse, helper, start, end;
+	int parse, helper, start, end;
 
-        if (array == NULL || size == 0)
-                return (-1);
-        start = 0;
-        end = size - 1;
+	if (array == NULL || size == 0)
+		return (-1);
+	start = 0;
+	end = size - 1;
 
-        while (start <= end)
-        {
-                printf("Searching in array: ");
-                for (parse = start; parse < end; parse++)
-                {
-                        printf("%d, ", *(array + parse));
-                }
-                printf("%d\n", *(array + parse));
-                helper = (start + end) / 2;
-                if (value > *(array + helper))
-                        start = helper + 1;
-                else if (value < *(array + helper))
-                        end = helper - 1;
-                else
-                        return (helper);
-        }
-        return (-1);
+	while (start <= end)
+	{
+		printf("Searching in array: ");
+		for (parse = start; parse < end; parse++)
+		{
+			printf("%d, ", *(array + parse));
+		}
+		printf("%d\n", *(array + parse));
+		helper = (start + end) / 2;
+		if (value > *(array + helper))
+			start = helper + 1;
+		else if (value < *(array + helper))
+			end = helper - 1;
+		else
+			return (helper);
+	}
+	return (-1);
 }
