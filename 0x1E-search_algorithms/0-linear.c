d75324a4994d13c8c620cbf43ae2c8d066ec2fd8
Kathleen McKiernan <kathleenmckiernan@UofNs-iMac-3.local>
2020-11-09 14:03:27 -0500

    sync for checker

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,6 +1,5 @@
 #include "search_algos.h"
 
-
 /**
  * linear_search - searches for value in array of ints with linear search
  * @array: first elem of array to search in
@@ -8,18 +7,19 @@
  * @value: value to search for
  * Return: if NULL -1
  */
+
 int linear_search(int *array, size_t size, int value)
 {
-        int parse = 0;
+	int parse = 0;
 
-        if (array == NULL)
-                return (-1);
+	if (array == NULL)
+		return (-1);
 
-        for (parse = 0; (size_t)parse < size; parse++)
-        {
-                printf("Value checked array[%i] = [%i]\n", parse, array[parse]);
-                if (array[parse] == value)
-                        return (parse);
-        }
-        return (-1);
+	for (parse = 0; (size_t)parse < size; parse++)
+	{
+		printf("Value checked array[%i] = [%i]\n", parse, array[parse]);
+		if (array[parse] == value)
+			return (parse);
+	}
+	return (-1);
 }
